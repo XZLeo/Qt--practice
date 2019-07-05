e0390e7506b032ebd6a2d7d99e5f48339a827a97
Leo Xiong <39155356+XZLeo@users.noreply.github.com>
2019-07-05 14:17:43 +0800

    Create main.cpp

---
+++ Clock/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include "digiclock.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DigiClock clock;
+    clock.show();
+
+    return a.exec();
+}
